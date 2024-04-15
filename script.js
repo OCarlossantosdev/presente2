0384a31d3b284029a7e137ed54b520a4f66d9a40
OCarlossantosdev <165340821+OCarlossantosdev@users.noreply.github.com>
2024-04-15 11:46:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  slides[slideIndex].classList.add('active');
+}
+
+prevBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
